886ce6b867600fe6c0aea5d40e4d448b313d4fff
Unknown <ak47trilogy@gmail.com>
2018-01-24 12:57:27 +0100

    fine

--- HTTP/posts.js
+++ HTTP/posts.js
@@ -1,14 +1,39 @@
-const input = require('./input')
+const data = require('./input/input');
+const id = data.prompt();
+const rp = require('request-promise');
 
-data.prompt();
+let arr = [];
 
-function request()
+function requestData()
 {
-
+    var options = {         //Object amibe vannak a request beállitasai
+        uri: 'https://jsonplaceholder.typicode.com/posts', //URL
+        json: true //Ezzel átkonvertálja a kapott adatokat automatikusan JSON-be
+    };
+    
+    (async function main(){ //asyn wrapper
+        
+       const response = await rp(options) //Tanultuk nemtom elmagyarázni
+       console.log(response) //Aki ezt a sort nem érti menjen haza!
+       for (let i = 0; i < response.length; i++) {
+            if (response[i].userId == id) {
+                arr.push(
+                    {
+                        title: response[i].title,
+                        body: response[i].body
+                    }
+                );
+            }
+       }
+    })()
 }
 
-function returnDatas()
+function returnData()
 {
-
+    module.exports = {
+        arr
+    };
 }
-//obj.type == "post" ? true : false;
+
+requestData();
+returnData();
