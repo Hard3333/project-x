46b09d0d2efcfedf5c5682c3863f1d6821af2767
GTDK06\DK06 <reif.oliver98@gmail.com>
2018-01-24 13:08:13 +0100

    Kész

--- HTTP/todos.js
+++ HTTP/todos.js
@@ -4,35 +4,27 @@ const rp = require('request-promise');
 
 let arr = [];
 
-function requestData()
-{
+async function requestData() {
     var options = {         //Object amibe vannak a request beállitasai
         uri: 'https://jsonplaceholder.typicode.com/todos', //URL
         json: true //Ezzel átkonvertálja a kapott adatokat automatikusan JSON-be
     };
-    
-    (async function main(){ //asyn wrapper
-        
-       const response = await rp(options) //Tanultuk nemtom elmagyarázni
-       console.log(response) //Aki ezt a sort nem érti menjen haza!
-       for (let i = 0; i < response.length; i++) {
-            if (response[i].userId == id) {
-                arr.push(
-                    {
-                        title: response[i].title
-                    }
-                );
-            }
-       }
-    })()
+
+    const response = await rp(options) //Tanultuk nemtom elmagyarázni
+    console.log(response) //Aki ezt a sort nem érti menjen haza!
+    for (let i = 0; i < response.length; i++) {
+        if (response[i].userId == id) {
+            arr.push(
+                {
+                    title: response[i].title
+                }
+            );
+        }
+    }
+
+    return arr;
 }
 
-function returnData()
-{
-    module.exports = {
-        arr
-    };
+module.exports = {
+    requestData
 }
-
-requestData();
-returnData();
